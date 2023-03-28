ed557e4cb516b6ac5e27e14bdb55328cca8efdbf
netpes <arbivnetanel@gmail.com>
2023-03-28 18:54:11 +0300

    add user type

--- Main/controllers/userController.ts
+++ Main/controllers/userController.ts
@@ -1,9 +1,19 @@
-import mongoose from 'mongoose'
-const User = require("../models/userModel")
-const Email = require("../models/EmailVerifyModel")
-const {sendEmail, passwordGenerator, changePassword, sendVerifyEmailAgain, checkEmail} = require("../assets/userFunctions")
-module.exports = {
-    Signup:(req,res)=>{
+// Essential imports
+import mongoose from "mongoose";
+const User = require("../models/userModel");
+const Email = require("../models/EmailVerifyModel");
 
-    }
-}
+// External Functions
+const {
+  sendEmail,
+  passwordGenerator,
+  changePassword,
+  sendVerifyEmailAgain,
+  checkEmail,
+} = require("../assets/userFunctions");
+// Types
+import { User } from "./types/userTypes";
+
+module.exports = {
+  Signup: (req, res) => {},
+};
