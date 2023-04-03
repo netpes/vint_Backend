e99dacfed292d6e621b327c5fa7f27d390fe0b8d
netpes <arbivnetanel@gmail.com>
2023-04-03 13:34:17 +0300

    analytics setup

---
+++ Analytics/server.ts
@@ -0,0 +1,30 @@
+// server imports:
+const express = require("express");
+const app = express();
+// external imports:
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = 5050 || process.env.PORT;
+//routes
+// const analyticsRouter = require("./routes/analytics");
+
+// .env file
+require("dotenv").config();
+
+// DB connection
+mongoose
+  .connect(process.env.DB, {})
+  .then(() => {
+    console.log("DB connect");
+  })
+  .catch(() => {
+    console.log("DB connect Failed");
+  });
+// essential server settings
+app.use(express.json());
+app.use(cors());
+
+// routes define
+// app.use("/", analyticsRouter);
+
+app.listen(PORT, () => console.log("connected: " + PORT));
