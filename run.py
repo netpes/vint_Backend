b5ab860781d1fb17d9279e8e6f258131358c4745
netpes <arbivnetanel@gmail.com>
2023-04-04 17:28:39 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,14 @@
 import subprocess
 
+
 class run():
-# you can run me by writing: py run.py
+    # you can run me by writing: py run.py
     install = "npm install"
     start = "npm start"
+
     def installPackages(self):
         process1 = subprocess.Popen(self.install, shell=True, cwd="./Main", stdout=subprocess.PIPE)
         process2 = subprocess.Popen(self.install, shell=True, cwd="./Analytics", stdout=subprocess.PIPE)
-        while True:
-            output1 = process1.stdout.readline().decode('utf-8').strip()
-            output2 = process2.stdout.readline().decode('utf-8').strip()
-            if output1 == '' and output2 == ''  and process1.poll() is not None and process2.poll() is not None:
-                break
-            if output1:
-                print(output1)
-            if output2:
-                print(output2)
-    def runEngine(self):
-        process1 = subprocess.Popen(start, shell=True, cwd="./Main", stdout=subprocess.PIPE)
-        process2 = subprocess.Popen(start, shell=True, cwd="./Analytics", stdout=subprocess.PIPE)
         while True:
             output1 = process1.stdout.readline().decode('utf-8').strip()
             output2 = process2.stdout.readline().decode('utf-8').strip()
@@ -29,7 +19,20 @@ class run():
             if output2:
                 print(output2)
 
-    def run(self):
+    def runEngine(self):
+        process1 = subprocess.Popen(self.start, shell=True, cwd="./Main", stdout=subprocess.PIPE)
+        process2 = subprocess.Popen(self.start, shell=True, cwd="./Analytics", stdout=subprocess.PIPE)
+        while True:
+            output1 = process1.stdout.readline().decode('utf-8').strip()
+            output2 = process2.stdout.readline().decode('utf-8').strip()
+            if output1 == '' and output2 == '' and process1.poll() is not None and process2.poll() is not None:
+                break
+            if output1:
+                print(output1)
+            if output2:
+                print(output2)
+
+    def __init__(self):
         while True:
             answer = input("Do you want to run npm i? (y/n) ").lower()
             if answer == "y":
@@ -39,8 +42,10 @@ class run():
                 break
             elif answer == "n":
                 print("starting the engine...")
-                runEngine()
+                self.runEngine()
                 break
             else:
                 print("Invalid input, please enter 'y' or 'n'")
 
+
+run()
