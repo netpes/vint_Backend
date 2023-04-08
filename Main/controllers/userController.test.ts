8f35028395b1020c6425d613451e03adf319131c
ravidro1 <ravidrrt@gmail.com>
2023-04-09 00:30:30 +0300

    add: functions of wishlist, products, following and tests

--- Main/controllers/userController.test.ts
+++ Main/controllers/userController.test.ts
@@ -24,139 +24,139 @@ test("checkEmail", () => {
   expect(checkEmail(toEmail)).toBe(true);
 });
 
-// test("sendEmail", () => {
-//   const subject = "subject";
-//   const html = "<h1></h1>";
-//   const toEmail = globalUser.email;
+test("sendEmail", () => {
+  const subject = "subject";
+  const html = "<h1></h1>";
+  const toEmail = globalUser.email;
 
-//   expect(sendEmail(subject, html, toEmail)).toBe(true);
-// });
+  expect(sendEmail(subject, html, toEmail)).toBe(true);
+});
 
-// test("passwordGenerator", () => {
-//   const newPassword = passwordGenerator(8);
-//   expect(newPassword.length).toBe(8);
-//   expect(typeof newPassword).toBe("string");
-// });
+test("passwordGenerator", () => {
+  const newPassword = passwordGenerator(8);
+  expect(newPassword.length).toBe(8);
+  expect(typeof newPassword).toBe("string");
+});
 
-// test("changePassword", async () => {
-//   const body = {
-//     userID: globalUserID,
-//     newPassword: "qwer",
-//   };
+test("changePassword", async () => {
+  const body = {
+    userID: globalUserID,
+    newPassword: "qwer",
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/changePassword`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/changePassword`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("sendVerifyEmailAgain", async () => {
-//   const body = {
-//     userID: globalUserID,
-//     email: globalUser.email,
-//   };
+test("sendVerifyEmailAgain", async () => {
+  const body = {
+    userID: globalUserID,
+    email: globalUser.email,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/sendVerifyEmailAgain`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/sendVerifyEmailAgain`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("signUp", async () => {
-//   const body: typeUser = { ...globalUser };
+test("signUp", async () => {
+  const body: typeUser = { ...globalUser };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/signUp`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/signUp`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("verifyEmail", async () => {
-//   const body = {
-//     userID: globalUserID,
-//     code: verifyEmailCode,
-//   };
+test("verifyEmail", async () => {
+  const body = {
+    userID: globalUserID,
+    code: verifyEmailCode,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/verifyEmail`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/verifyEmail`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("userInfo", async () => {
-//   const body = {
-//     userID: globalUserID,
-//   };
+test("userInfo", async () => {
+  const body = {
+    userID: globalUserID,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/userInfo`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/userInfo`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("login", async () => {
-//   const body = {
-//     username: globalUser.username,
-//     password: globalUser.password,
-//   };
+test("login", async () => {
+  const body = {
+    username: globalUser.username,
+    password: globalUser.password,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/login`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/login`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("changeEmail", async () => {
-//   const body = {
-//     userID: globalUserID,
-//     newEmail: globalUser.email,
-//   };
+test("changeEmail", async () => {
+  const body = {
+    userID: globalUserID,
+    newEmail: globalUser.email,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/changeEmail`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/changeEmail`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("forgotPassword", async () => {
-//   const body = {
-//     username: globalUser.username,
-//   };
+test("forgotPassword", async () => {
+  const body = {
+    username: globalUser.username,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/forgotPassword`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/forgotPassword`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
-// test("deleteAccount", async () => {
-//   const body = {
-//     userID: globalUserID,
-//   };
+test("deleteAccount", async () => {
+  const body = {
+    userID: globalUserID,
+  };
 
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/deleteAccount`,
-//     body
-//   );
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/deleteAccount`,
+    body
+  );
 
-//   expect(res).toBeTruthy();
-// });
+  expect(res).toBeTruthy();
+});
 
 test("verifyToken", async () => {
   const body = {
@@ -170,8 +170,146 @@ test("verifyToken", async () => {
     body
   );
 
+  console.log(res);
+
   expect(res.status).toBe(200);
   expect(res.data).toEqual({ message: "Verify Token", verify: true });
+});
+
+test("getUserById", async () => {
+  const body = {
+    userID: globalUserID,
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/getUserById`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("getFollowingList", async () => {
+  const body = {
+    userID: globalUserID,
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/getFollowingList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("addSellerToFollowingList", async () => {
+  const body = {
+    userID: globalUserID,
+    newFollowingID: "6412b66799d3a23e507dcdd0",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/addSellerToFollowingList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("removeSellerFromFollowingList", async () => {
+  const body = {
+    userID: globalUserID,
+    idToRemove: "6412b66799d3a23e507dcdd0",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/removeSellerFromFollowingList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("getUserProductsList", async () => {
+  const body = {
+    userID: globalUserID,
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/getUserProductsList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("addProductToUserProductsList", async () => {
+  const body = {
+    userID: globalUserID,
+    productID: "6412c802a259648cb1fb64ed",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/addProductToUserProductsList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("removeProductFromUserProductsList", async () => {
+  const body = {
+    userID: globalUserID,
+    productIDToRemove: "6412b66799d3a23e507dcdd0",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/removeProductFromUserProductsList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("getWishList", async () => {
+  const body = {
+    userID: globalUserID,
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/getWishList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("addToWishList", async () => {
+  const body = {
+    userID: globalUserID,
+    productID: "6412c802a259648cb1fb64ed",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/addToWishList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
+});
+
+test("removeFromWishList", async () => {
+  const body = {
+    userID: globalUserID,
+    productIDToRemove: "6412b66799d3a23e507dcdd0",
+  };
+
+  const res = await axios.post(
+    `${process.env.BACKEND_URL_FOR_TESTS}user/removeFromWishList`,
+    body
+  );
+
+  expect(res.status).toBe(200);
 });
 
 // test("changeProfilePicture", async () => {
@@ -179,40 +317,3 @@ test("verifyToken", async () => {
 //     userID: globalUserID,
 //     image:""
 //   };
-
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/changeProfilePicture`,
-//     body
-//   );
-
-//   expect(res).toBeTruthy();
-// });
-
-// test("getWishList", async () => {
-//   const body = {
-//     userID: globalUserID,
-//   };
-
-//   const res = await axios.post(
-//     `${process.env.BACKEND_URL_FOR_TESTS}user/getWishList`,
-//     body
-//   );
-
-//   expect(res).toBeTruthy();
-// });
-
-// test("addToWishList", async () => {});
-
-// test("removeFromWishList", async () => {});
-
-// test("addProductToUserProductsList", async () => {});
-// test("removeProductFromUserProductsList", async () => {});
-// test("getUserProductsList", async () => {});
-
-// test("addSellerToFollowingList", async () => {});
-
-// test("removeSellerFromFollowingList", async () => {});
-
-// test("getFollowingList", async () => {});
-
-// test("getUserById", async () => {});
